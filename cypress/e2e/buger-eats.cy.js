8bf0b201f840a8f0336d35df1aad443bd4fd4f47
Marco Pla <marco.pla@gmail.com>
2022-09-01 23:58:39 -0300

    button return test

--- cypress/e2e/buger-eats.cy.js
+++ cypress/e2e/buger-eats.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Cadastro de entregador ', () => {
+  beforeEach(() => {
+    cy.visit('https://buger-eats.vercel.app/');
+  });
+
+  it('verifica se o botao de cadastro esta funcionando', () => {
+    cy.contains('a', 'Cadastre-se para fazer entregas').click();
+    cy.get('h1').should('contain', 'Cadastre-se');
+  });
+
+  it('verificar se botão de retorno para a home funciona.', () => {
+    cy.contains('a', 'Cadastre-se para fazer entregas').click();
+    cy.get('h1').should('contain', 'Cadastre-se');
+  });
+});
